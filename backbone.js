cadfcd7a706e2571cc8b9870cb58505a50ce2ef6
Srikumar K. S <srikumarks@gmail.com>
2012-05-03 12:24:28 +0800

    added package for backbone

---
+++ backbone.js
@@ -0,0 +1,16 @@
+// A "packaged" version of Backbone that doesn't
+// pollute the global namespace with a "Backbone".
+// The "backbone-0.9.2.js" file needs to co-exist
+// with this package in this build directory.
+// 
+// WARNING: Not intended for use directly within
+// a browser. More for use when building a minified
+// application.
+_package('backbone', 
+        eval('(function () {\n' 
+            + 'var module = {exports: {}}, exports = module.exports;\n'
+            + 'return (function () {\n'
+            + _package.fetch('backbone-0.9.2.js')
+            + '\nreturn this["Backbone"] || module.exports;\n'
+            + '}).call({});\n'
+            + '})\n'));
