0f5803eff2fed714b2f10be1466d3a3e1ae2c973
Srikumar K. S <srikumarks@gmail.com>
2012-05-24 18:31:09 +0800

    When a package has no configuration, a check for the "external" property must yield 'false' instead of a property access on undefined.

--- package.js
+++ package.js
@@ -236,7 +236,7 @@
             addOnLoad(name, callback);
             if (!pseudoPackage(name)) {
                 var cfg = findConfig(name);
-                if (cfg.external) {
+                if (cfg && cfg.external) {
                     loadExternalModuleFromURL(name, cfg.external.url, cfg.external.dependsOn, cfg.external.depNames, cfg.external.name);
                 } else {
                     var script = document.createElement('script');
@@ -262,7 +262,7 @@
             addOnLoad(name, callback);
             if (!pseudoPackage(name)) {
                 var cfg = findConfig(name);
-                if (cfg.external) {
+                if (cfg && cfg.external) {
                     loadExternalModuleFromURL(name, cfg.external.url, cfg.external.dependsOn, cfg.external.depNames, cfg.external.name);
                 } else {
                     where = packageURL(name);
